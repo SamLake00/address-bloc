72127691bc994da8cd957de9eaa43f29c7a90417
samlake00 <samuelalake1@gmail.com>
2018-05-30 23:39:28 +0000

    iterative search

--- models/address_book.rb
+++ models/address_book.rb
@@ -20,7 +20,7 @@ require 'csv'
             entries.insert(index, Entry.new(name, phone_number, email))
         end
         
-        def remove_entry(name, phone_number, email)
+        def remove_entry(name = "", phone_number = "", email = "")
             index = 0
             entries.each do |entry|
                 if name == entry.name || phone_number == entry.phone_number || email == entry.email
@@ -41,5 +41,42 @@ require 'csv'
                 row_hash = row.to_hash
                 add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
             end
+        end
+        
+        def binary_search(name)
+            lower = 0
+            upper = entries.length - 1
+           
+            while lower <= upper
+                mid = (upper + lower) / 2
+                mid_name = entries[mid].name
+                
+                if name == mid_name
+                    return entries[mid]
+                elsif name <= mid_name
+                    upper = mid - 1
+                elsif name >= mid_name
+                    lower = mid + 1
+                end
+            end
+            
+            return nil
+        end
+        
+        def iterative_search(name)
+            lower = 0
+            upper = entries.length - 1
+            
+            while lower <= upper
+                iterative_name = entries[lower].name
+                
+                if name == iterative_name
+                    return entries[lower]
+                else
+                    lower += 1
+                end
+            end
+            
+            return nil
         end
     end
